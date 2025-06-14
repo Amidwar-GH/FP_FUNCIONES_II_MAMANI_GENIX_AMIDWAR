e00603ff273916a11b3de904997583066e522282
Amidwar <amidwar05@gmail.com>
2025-06-14 08:51:30 -0500

    EJERCICIO6- AVANCE: FINALIZANDO EJERCICIO, SE AGREGO EL ALGORITMO DEL JUEGO DE PIEDRA PAPEL O TIJERA

--- EJERCICIO-06.cpp
+++ EJERCICIO-06.cpp
@@ -28,9 +28,56 @@ int main(){
 		
 		CPU = rand() % 3 + 1;
 		
+		cout<<"Elegiste: ";
+		if(JUGADA == 1){
+			cout<<"PIEDRA";
+		}else if(JUGADA == 2 ){
+			cout<<"PAPEL";
+		}else if(JUGADA == 3){
+			cout<<"TIJERA";
+		}
+		cout<<endl;
+			
+		cout<<"CPU eligio: ";
+		if(CPU == 1){
+			cout<<"PIEDRA";
+		}else if(CPU == 2 ){
+			cout<<"PAPEL";
+		}else if(CPU == 3){
+			cout<<"TIJERA";
+		}
+		cout<<endl;
+		
+		if(JUGADA == CPU){
+			cout<<"EMPATE"<<endl;
+		}else if(JUGADA == 1 && CPU == 3){
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+			VICTORIAS_DEL_USUARIO += 1;
+		}else if(JUGADA == 2 && CPU == 1){
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+			VICTORIAS_DEL_USUARIO += 1;
+		}else if(JUGADA == 3 && CPU == 2){
+			cout<<"FELICIDADES, GANASTE ESTA RONDA."<<endl;
+			VICTORIAS_DEL_USUARIO += 1;
+		}else{
+			cout<<"PERDISTE ESTA RONDA."<<endl;
+			VICTORIAS_DEL_CPU +=1;
+		}
+		
+		cout<<endl;
+		cout<<NOMBRE<<" tu puntaje es "<<VICTORIAS_DEL_USUARIO<<endl;
+		cout<<"El puntaje del CPU es "<<VICTORIAS_DEL_CPU<<endl;
+		cout<<endl;
+		
+		
 	}while( VICTORIAS_DEL_USUARIO<3 && VICTORIAS_DEL_CPU<3);
 	
 	
+	if(VICTORIAS_DEL_USUARIO == 3 ){
+		cout<<"FELICIDADES, GANASTE EL JUEGO."<<endl;
+	}else{
+		cout<<"SUERTE PARA LA PROXIMA. ";
+	}
 	
 	return 0;
 }
