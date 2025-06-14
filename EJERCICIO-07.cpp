f2c08e6afdb6be8b0016a2d43df75f9d446668bb
Amidwar <amidwar05@gmail.com>
2025-06-14 09:21:15 -0500

    EJERCICIO7-AVANCE: AGREGANDO EL EJERCICIO 7, IMPLEMENTANDO LIBRERIAS Y UN AVANZE GENERAL DEL PROGRAMA

---
+++ EJERCICIO-07.cpp
@@ -0,0 +1,34 @@
+/*7. Juego de Dados por Turnos: Desarrolla un programa que simule un juego entre dos jugadores que 
+lanzan un dado por turnos. Cada dado genera un número aleatorio entre 1 y 6. El jugador que 
+obtenga el número mayor gana la ronda. El primero en ganar 3 rondas será el ganador del juego. 
+Muestra el resultado de cada ronda y el marcador acumulado. Usa funciones y números aleatorios 
+(rand de cstdlib).*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	int JUGADOR_A, JUGADOR_B;
+	int VICTORIAS_A, VICTORIAS_B;
+	srand(time(0));
+	
+	do{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}while(VICTORIAS_A <3 && VICTORIAS_B <3);
+	
+	
+	
+	
+	
+	return 0;
+} 
