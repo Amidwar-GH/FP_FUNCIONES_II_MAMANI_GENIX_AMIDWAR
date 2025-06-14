37c0a714469f0a58acf1560d648b2ffe2e7dbf45
Amidwar <amidwar05@gmail.com>
2025-06-14 10:30:49 -0500

    EJERCICIO8-AVANCE: AGREGANDO EL EJERCICIO 8, AGREGANDO LIBRERIAS Y ESTRUCTURA PRINCIPAL

---
+++ EJERCICIO-08.cpp
@@ -0,0 +1,22 @@
+/*8. Simulación de Tragamonedas Simple: Crea un programa que simule una máquina tragamonedas. 
+El usuario inicia con 100 monedas y puede apostar entre 1 y 10 por turno. Se generan tres números 
+aleatorios entre 1 y 5. Si los tres son iguales, gana 10 veces su apuesta. Si hay dos iguales, gana el 
+doble. Si forman una escalera (por ejemplo como 2-3-4 o 4-3-2), gana 5 veces su apuesta. En 
+cualquier otro caso, pierde lo apostado. El juego continúa hasta que el jugador se quede sin monedas 
+o decida salir. Usa funciones y números aleatorios*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
