b51dbab6037d30ebaecf8424e81721ab833522ee
Amidwar <amidwar05@gmail.com>
2025-06-15 10:29:46 -0500

    EJERCICIO5-AVANCE: Agregando el quinto ejercicio, agregando librerias, agregando funcion principal y variables

---
+++ EJERCICIO-05.cpp
@@ -0,0 +1,20 @@
+/*5. Función calcularRaices: Amplía el ejercicio anterior definiendo una función void llamada 
+calcularRaices que reciba los coeficientes de la ecuación (a, b, c) por valor y las variables donde 
+se almacenarán las raíces por referencia. Además, incluye una variable por referencia que indique si 
+las raíces son reales o complejas. La función debe calcular las raíces utilizando la fórmula general y 
+actualizar las variables correspondientes.*/
+
+#include<iostream>
+#include<cmath>
+
+int main(){
+	int a,b,c,reales;
+	cout<<"Digite el coeficiente del termino cuadratico: "; cin>>a;
+	cout<<"Digite el coeficiente del termino lineal: "; cin>>b;
+	cout<<"Digite el termino independiente: "; cin>>c;
+	
+	
+	
+	
+	return 0;
+}
