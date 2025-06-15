033305d678c5579c1048c47416dee76155cac7d4
Amidwar <amidwar05@gmail.com>
2025-06-14 22:42:38 -0500

    EJERCICIO3-AVANCE: Agregando el tercer ejercicio, agregando librerias

---
+++ EJERCICIO-03.cpp
@@ -0,0 +1,13 @@
+/*3. Cálculo de la Hipotenusa: Crea un programa que calcule la hipotenusa de un triángulo rectángulo 
+utilizando la fórmula de Pitágoras. Solicita al usuario los valores de los catetos y utiliza la función 
+sqrt de cmath para calcular la raíz cuadrada de la suma de los cuadrados de los catetos. Muestra el 
+resultado de la hipotenusa calculada.*/
+
+//H^2=c^2+c^2
+
+#include<iostream>
+#include<cmath>
+
+using namespace std; 
+
+
