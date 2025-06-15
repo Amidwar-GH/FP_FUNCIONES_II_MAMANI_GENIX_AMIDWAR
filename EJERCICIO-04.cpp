9e45a49159ebd50c415da7970704cd1f190b9161
Amidwar <amidwar05@gmail.com>
2025-06-14 23:35:11 -0500

    EJERCICIO4-AVANCE: Agregando variables, agregando formula de la discriminante, haciendo llamado de funciones, falta por implementar las funcines

--- EJERCICIO-04.cpp
+++ EJERCICIO-04.cpp
@@ -14,8 +14,28 @@ void ENTRADA(){
 }
 
 int main(){
+	float a,b,c,DISCRIMINANTE,R1,R2;
+	
 	
 	ENTRADA();
+	cout<<endl;
+	
+	cout<<"Digite el coeficiente  del termino cuadratico: "; cin>>a;
+	cout<<"Digite el coeficiente del termino lineal: "; cin>>b;
+	cout<<"Digite el termino independiente: "; cin>>c;
+	
+	DISCRIMINANTE= pow(b,2)-4*a*c;
+	
+	if(DISCRIMINANTE<0){
+		cout<<"La ecuacion tiene raizes complejas.";
+	}else{
+		cout<<"La discriminante es "<<DISCRIMINANTE<<endl;
+		R1=RAIZ1(a,b,c);
+		cout<<"La primera raiz es "<<R1<<endl;;
+		R2=RAIZ2(a,b,c);
+		cout<<"La segunda raiz es "<<R2<<endl;
+	}
+	
 	return 0;
 }
  
