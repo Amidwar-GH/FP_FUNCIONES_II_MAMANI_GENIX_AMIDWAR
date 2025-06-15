2730854e9ea4f114a0a66f9258a2e168f4eb4fda
Amidwar <amidwar05@gmail.com>
2025-06-14 22:58:07 -0500

    EJERCICIO3-Calaculo de hipotenusa (VERSION FINAL)

--- EJERCICIO-03.cpp
+++ EJERCICIO-03.cpp
@@ -11,3 +11,43 @@ resultado de la hipotenusa calculada.*/
 using namespace std; 
 
 
+float calculodeHIPOTENUSA(int cateto1, int cateto2){
+	float HIPOTENUSA;
+	HIPOTENUSA=sqrt((pow(cateto1,2))+(pow(cateto2,2)));
+	return HIPOTENUSA;
+}
+
+
+void bienvenida(){
+	cout<<"-----------------------"<<endl;
+	cout<<"BIENVENIDO AL PROGRAMA."<<endl;
+	cout<<"-----------------------"<<endl;
+}
+
+
+int main(){
+	float cateto1,cateto2,H;
+	
+	bienvenida();
+	cout<<"                                                           "<<endl;
+	
+	//no hay lados del triangulo negativos tampoco lados del triangulo ceros.
+	cout<<"Digite el primer cateto del triangulo rectangulo : "; cin>>cateto1;
+	while(cateto1<=0){
+		cout<<"Digite nuevamente el primer cateto del triangulo: ";
+		cin>>cateto1;
+	}
+	
+	
+	cout<<"Digite el segundo cateto del triangulo rectangulo: "; cin>>cateto2;
+	while(cateto2<=0){
+		cout<<"Digite nuevamente el segundo cateto del triangulo: ";
+		cin>>cateto2;
+	}
+	
+	
+	H=calculodeHIPOTENUSA(cateto1,cateto2);
+	cout<<"La hipotenusa del triangulo rectangulo es "<<H;
+	
+	return 0;
+}
