d05674bbc714fc07668d047c072df603ee5078c4
Amidwar <amidwar05@gmail.com>
2025-06-14 20:36:23 -0500

    EJERCICIO1-AVANCE: AGREGANDO FUNCION PRINCIPAL LIBRERIAS Y VARIABLES

---
+++ EJERCICIO-01.cpp
@@ -0,0 +1,17 @@
+/*1. Cálculo del Área de un Círculo: Implementa un programa que calcule el área de un círculo dado su 
+radio. Utiliza la constante M_PI de la librería cmath para obtener el valor de pi. Solicita al usuario el 
+radio del círculo y utiliza la función pow de cmath para calcular el cuadrado del radio. Muestra el 
+resultado del área calculada*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	float r,Areadelcirculo;
+	cout<<"Digite el radio del circulo: "; cin>>r;
+	
+	Areadelcirculo=FuncionCirculo(r);
+	cout<<"El area del circulo es "<<Areadelcirculo;
+	return 0;
+}
